35e7f462e187730787977bbe29d3d349c2eee835
John Romanski <jromanski@aegiscomm.com>
2011-05-24 13:10:22 -0400

    updated email regex

--- lib/ValidateEmail.rb
+++ lib/ValidateEmail.rb
@@ -2,10 +2,9 @@ require 'resolv'
 
 module ValidateEmail
   def self.validate(email, validate_mx = false)
-    email_pattern = (email =~ /^[a-zA-Z][\w\.-]*[a-zA-Z0-9]@[a-zA-Z0-9][\w\.-]*[a-zA-Z0-9]\.[a-zA-Z][a-zA-Z\.]*[a-zA-Z]$/)
+    email_pattern = (email =~ /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i)
     is_valid = email_pattern.nil? ? false : true
     is_valid = validate_mx_record(email) if is_valid && validate_mx
-
     return is_valid
   end
 
